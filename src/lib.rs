d76a183015d54be681f37480c6905673d21c0b89
Joost van der Laan <j.vanderlaan1@students.uu.nl>
2024-02-10 00:52:30 +0100

    Update Top-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 //! Parses a linear programming problem from a string using [parser] and finds the optimal solution
 //! using [solver].\
 //! \
-//! Every line will need to contain the same amount of variables and in the same order (the parser is blind for names)
+//! Every line will need to contain the same amount of variables and in the same order (the parser is blind for names).
+//! The first line is read to be the linear objective function while all other lines are constraints of the form: f >= 0 \
 //! The solver will keep rewriting the system until it can read out the solution. You can also directly create a [SystemEq](solver::SystemEq) and solve that.\
 //! \
 //! It is assumed that every variable cannot be negative.
 //! 
 //! # Example
+//! Remember that the first line is the objective function
 //! ```
 //! let input ="1 + 2x
 //!             4 - 2x
